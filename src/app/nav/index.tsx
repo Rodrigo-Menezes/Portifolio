3c945c1c8613651f8cce4c32b119b8b5c18a4e35
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-07-18 17:56:55 -0300

    correcão

--- src/app/nav/index.tsx
+++ src/app/nav/index.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
 
   return (
     <div className="bg-slate-900 z-20 fixed w-screen hidden md:flex justify-between items-center border-b border-prata h-20">
-      <div className='flex items-center pl-10 drop-shadow-dark3'>
+      <div className='flex items-center pl-10 drop-shadow-dark3 '>
         <Image src={rodrigo} alt='logo rodrigo menezes 'width={85} />
       </div>
       <div className="flex justify-center items-center gap-12 text-prata">
