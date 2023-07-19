224a20ba91cb18ee5cafcc4cb005baef818849d0
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-07-19 13:41:05 -0300

    att nos icones dos cards

--- src/app/conteudo/sobre/Card.tsx
+++ src/app/conteudo/sobre/Card.tsx
@@ -1,5 +1,4 @@
 interface Conteudo {
-  icon:string;
   number: number;
   text: string;
  
@@ -7,9 +6,6 @@ interface Conteudo {
 export default function Card(ConteudoOBJ: Conteudo) {
   return (
     <div className="group w-32 md:w-40 md:h-28 bg-white bg-opacity-5 hover:bg-opacity-20 transition ease-in-out duration-1000 text-prata rounded-2xl flex flex-col ">
-      <div className="px-3 pt-3 ">
-       {ConteudoOBJ.icon}
-      </div>
       <div className="px-3 py-3 flex flex-col text-sm relative items-end h-full overflow-hidden justify-between">
         {ConteudoOBJ.text}
         <div className="md:absolute right-2 pl-2 text-xl md:text-4xl group-hover:scale-200  group-transition ease-in duration-500 group-hover:-rotate-45 z-10">
