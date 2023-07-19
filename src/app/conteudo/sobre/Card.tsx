2bbbeb63e471b15049d2a9e8b382caee62930164
Rodrigo Menezes <rodrigo_menezes91@hotmail.com>
2023-07-19 13:33:15 -0300

    att

--- src/app/conteudo/sobre/Card.tsx
+++ src/app/conteudo/sobre/Card.tsx
@@ -1,4 +1,5 @@
 interface Conteudo {
+  icon:string;
   number: number;
   text: string;
  
@@ -7,7 +8,7 @@ export default function Card(ConteudoOBJ: Conteudo) {
   return (
     <div className="group w-32 md:w-40 md:h-28 bg-white bg-opacity-5 hover:bg-opacity-20 transition ease-in-out duration-1000 text-prata rounded-2xl flex flex-col ">
       <div className="px-3 pt-3 ">
-       
+       {ConteudoOBJ.icon}
       </div>
       <div className="px-3 py-3 flex flex-col text-sm relative items-end h-full overflow-hidden justify-between">
         {ConteudoOBJ.text}
